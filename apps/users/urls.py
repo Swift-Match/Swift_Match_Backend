f73236015d75e6f686d0e7a0926b876b56ae958a
Natália Rodrigues <natytotherodrigues@gmail.com>
2025-11-23 12:37:00 -0300

    feat(users): add endpoints for user theme and first login

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from .views import RegisterUserView, UserProfileView
+from .views import (
+    RegisterUserView, 
+    UserProfileView, 
+    UserThemeUpdateView, 
+    UserThemeListView,
+    UserFirstLoginView
+)
 
 urlpatterns = [
     # POST para /api/users/register/
     path('register/', RegisterUserView.as_view(), name='user-register'),
     path('me/', UserProfileView.as_view(), name='user-profile'),
+    path('me/theme/', UserThemeUpdateView.as_view(), name='user-update-theme'),  # PUT/PATCH
+    path('themes/', UserThemeListView.as_view(), name='user-list-themes'),       # GET
+    path('me/first-login/', UserFirstLoginView.as_view(), name='user-first-login'),
 ]
