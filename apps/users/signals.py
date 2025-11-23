6f48e0e9f90c4d9145c364d0454100d0b47840b9
Natália Rodrigues <natytotherodrigues@gmail.com>
2025-11-23 12:36:32 -0300

    feat(users): track user's first login with user_logged_in signal

---
+++ apps/users/signals.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.signals import user_logged_in
+from django.utils import timezone
+
+def set_first_login(sender, request, user, **kwargs):
+    if not user.first_login:
+        user.first_login = timezone.now()
+        user.save()
+
+user_logged_in.connect(set_first_login)
