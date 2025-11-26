e0beb1de90637689055e36b8cefde55ea82b9a8f
Natália Rodrigues <natytotherodrigues@gmail.com>
2025-11-26 19:29:40 -0300

    from django.apps import AppConfig
    class RankingsConfig(AppConfig):
        default_auto_field = 'django.db.models.BigAutoField'
        name = 'apps.rankings'
    
        def ready(self):
            # importa o módulo signals para registrar receivers
            try:
                import apps.rankings.signals  # noqa: F401
            except Exception:
                # opcional: logar erro para debug
                import logging
                logging.getLogger(__name__).exception("Falha importando apps.rankings.signals")

--- apps/rankings/apps.py
+++ apps/rankings/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 class RankingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.rankings'
+
+    def ready(self):
+        # importa o módulo signals para registrar receivers
+        try:
+            import apps.rankings.signals  # noqa: F401
+        except Exception:
+            # opcional: logar erro para debug
+            import logging
+            logging.getLogger(__name__).exception("Falha importando apps.rankings.signals")
