249cfe8497bcedcbb70b6dff413f4ae95457e6ee
Natália Rodrigues <natytotherodrigues@gmail.com>
2025-11-27 01:59:38 -0300

    add: removing 'exec' just to test at render

--- run_prod.sh
+++ run_prod.sh
@@ -1,18 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-# run_prod.sh
-
-# 1. Aplicar Migrações do Banco de Dados
 echo "--> Aplicando migrações no Supabase..."
 python manage.py migrate --noinput
 
-# Verificar se a migração falhou.
-if [ $? -ne 0 ]; then
-    echo "Falha na aplicação das migrações. Saindo."
-    exit 1
-fi
+echo "--> Iniciando Celery Worker e Beat em segundo plano..."
+celery -A config worker -l info &
+celery -A config beat -l info -s /tmp/celerybeat-schedule &
 
-# 2. Iniciar o Servidor Web Gunicorn (produção)
-echo "--> Iniciando Gunicorn em $PORT..."
-# Usa o Gunicorn e a porta fornecida pelo Render
-exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
+echo "--> Iniciando Gunicorn em primeiro plano (Sem 'exec')..."
+# REMOVA O 'exec' AQUI!
+gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
