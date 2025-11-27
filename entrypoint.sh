78199e0eac367f33e583efac9b34423d0d083828
Natália Rodrigues <natytotherodrigues@gmail.com>
2025-11-27 02:28:48 -0300

    fix: using "run_prod.sh" inconditionaly in production

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 if [ -z "$RENDER" ]; then
     echo "🚀 Aguardando banco de dados local (db:5432)..."
     /usr/local/bin/wait-for-it.sh db:5432 --timeout=60 --strict -- echo "Database UP! ✔️"
+    exec "$@" 
 else
-    echo "Ambiente de Produção (Render) detectado. Pulando wait-for-it."
+    echo "Ambiente de Produção (Render) detectado."
+    echo "--> Executando script de PRODUÇÃO (run_prod.sh) [COM MIGRAÇÃO E CELERY]"
+    exec /app/run_prod.sh
 fi
-
-echo "--> Executando comando de inicialização fornecido pela Render: $@"
-exec "$@"
